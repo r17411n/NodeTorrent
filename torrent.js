516d1e40c0c7f84652771ac48d9a3a762723b1c8
MAGACAT <magacatzz@proton.me>
2025-11-15 22:23:28 +0100

    removed non needed lines

--- torrent.js
+++ torrent.js
@@ -6,38 +6,33 @@ import { exec } from 'child_process';
 let port = 8888;
 let client = new WebTorrent();
 let percentages = [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
-let magnetURI = 'magnet:?xt=urn:btih:9A4D23FA7A824C11D8EF363EA8FAFFDFCD2D62C9&dn=Frankenstein.2025.1080p.WEBRip.10Bit.DDP5.1.x265-NeoNoir&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=http%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.ololosh.space%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dump.cl%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker-udp.gbitt.info%3A80%2Fannounce&tr=udp%3A%2F%2Fretracker01-msk-virt.corbina.net%3A80%2Fannounce&tr=udp%3A%2F%2Fopen.free-tracker.ga%3A6969%2Fannounce&tr=udp%3A%2F%2Fns-1.x-fins.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce';
+let magnetURI = 'magnet:?xt=urn:btih:6397292FC9E95611D94A1EBCD1FCBF4ACEE449B8&dn=Tantura.1080p.HC.WEB-DL.AAC2.0.x264.mkv&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2850%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce';
 
 if (Number.isInteger(port) === false) {
-
   console.error('Invalid port number. Using default port 8888.');
   port = 8888;
 } else {
-
   console.log('Using port:', port);
 }
 
 console.log('Adding Magnet Link to play queue:',magnetURI.substring(0, 120),'...');
 
 client.add(magnetURI, torrent => {
-  
   let file = torrent.files.find(f => f.name.endsWith('.mp4') || f.name.endsWith('.mkv'));
+
   console.log('Torrent info hash:', torrent.infoHash);
  
   if (!file) {
-
     console.error('No playable video file inside the torrent.');
     return;
   }
   
   let server = http.createServer((req, res) => {
-    
     let range = req.headers.range;
     let fileSize = file.length;
     let mimeType = mime.getType(file.name) || 'video/mp4';
     
     if (!range) {
-      
       let stream = file.createReadStream({ start, end });
       
       res.writeHead(200, {'Content-Length': fileSize, 'Content-Type': mimeType});
@@ -56,7 +51,6 @@ client.add(magnetURI, torrent => {
 
       stream.pipe(res);
     } else {
-
       let [startStr, endStr] = range.replace(/bytes=/, '').split('-');
       let start = parseInt(startStr, 10);
       let end = endStr ? parseInt(endStr, 10) : fileSize - 1;
@@ -71,24 +65,20 @@ client.add(magnetURI, torrent => {
       });
 
       req.on('start', () => {
-
         console.log('Client connected');
       });
 
       stream.on('error', err => {
-
         console.error('Stream error:', err);
         res.end();
       });
 
       req.on('close', () => {
-
         console.log('Client disconnected');
         stream.destroy();
       });
 
       torrent.on('download', () => {
-
         let progress = (torrent.progress * 100).toFixed(2);
         let uploaded = (torrent.uploaded / 1024).toFixed(2);
         let downloaded = (torrent.downloaded / 1024).toFixed(2);
@@ -101,31 +91,26 @@ client.add(magnetURI, torrent => {
         if (percentages.includes(progress)) {
 
           if (typeof seeders === 'undefined') {
-
             seeders = 0;
           }
           console.log(`Progress: ${progress}% - Speed: ${speeds}`);
           console.log(`Uploaded: ${(uploaded/1024).toFixed(2)} MB - Downloaded: ${(downloaded/1024).toFixed(2)} MB - Ratio: ${ratio} - Peers: ${peers}`);
         }
       });
-
       stream.pipe(res);
     }
   });
 
   server.listen(port, () => {
-
     console.log(`Streaming on http://localhost:${port}`);
     console.log('Launching VLC player...');
 
     exec(`vlc --network-caching=1000 http://localhost:${port}`, err => {
-
       if (err) {
         console.error('Failed to launch VLC:', err);
       } else {
         console.log('VLC player closed');
       }
-
     });
   });
 });
